9c0609714c32fa4dfd8738350fad7eecd207ce0a
shinedi <shinedi@foxmail.com>
2020-01-02 22:35:09 +0800

    初始化

---
+++ src/main/java/com/shinedi/java/test.java
@@ -0,0 +1,18 @@
+package com.shinedi.java;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class test extends Application {
+
+    public static void main(String[] args) {
+
+        System.out.println("hallo word");
+        return;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
